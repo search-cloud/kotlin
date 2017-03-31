c665e3764b673090ee3ddab3518dbae2828eb9ed
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-03-31 17:39:03 +0300

    Use null instead of CompilerMessageLocation.NO_LOCATION in MessageCollector

--- plugins/source-sections/source-sections-compiler/tests/org/jetbrains/kotlin/sourceSections/SourceSectionsTest.kt
+++ plugins/source-sections/source-sections-compiler/tests/org/jetbrains/kotlin/sourceSections/SourceSectionsTest.kt
@@ -262,8 +262,7 @@ internal fun<T> captureOut(body: () -> T): Pair<String, T> {
 }
 
 class TestMessageCollector : MessageCollector {
-
-    data class Message(val severity: CompilerMessageSeverity, val message: String, val location: CompilerMessageLocation)
+    data class Message(val severity: CompilerMessageSeverity, val message: String, val location: CompilerMessageLocation?)
 
     val messages = arrayListOf<Message>()
 
@@ -271,7 +270,7 @@ class TestMessageCollector : MessageCollector {
         messages.clear()
     }
 
-    override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageLocation) {
+    override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageLocation?) {
         messages.add(Message(severity, message, location))
     }
 
